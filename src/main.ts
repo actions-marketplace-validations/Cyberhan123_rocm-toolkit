df673c35b777a1b476b1cf9fef71c57dd6378012
Cyberhan123 <255542417@qq.com>
2023-11-30 23:20:46 +0800

    fix ci fail

--- src/main.ts
+++ src/main.ts
@@ -1,113 +1,47 @@
 import * as core from '@actions/core'
-import {Method, parseMethod} from './method'
-import {OSType, getOs} from './platform'
-import {aptInstall, aptSetup, useApt} from './apt-installer'
 import {download} from './downloader'
 import {getVersion} from './version'
 import {install} from './installer'
 import {updatePath} from './update-path'
-import {parsePackages} from './parser'
 
 async function run(): Promise<void> {
-  try {
-    const rocm: string = core.getInput('rocm')
-    core.debug(`Desired cuda version: ${rocm}`)
-    const subPackagesArgName = 'sub-packages'
-    const subPackages: string = core.getInput(subPackagesArgName)
-    core.debug(`Desired subPackages: ${subPackages}`)
-    const nonCudaSubPackagesArgName = 'non-cuda-sub-packages'
-    const nonCudaSubPackages: string = core.getInput(nonCudaSubPackagesArgName)
-    core.debug(`Desired nonCudasubPackages: ${nonCudaSubPackages}`)
-    const methodString: string = core.getInput('method')
-    core.debug(`Desired method: ${methodString}`)
-    const linuxLocalArgs: string = core.getInput('linux-local-args')
-    core.debug(`Desired local linux args: ${linuxLocalArgs}`)
-    const useGitHubCache: boolean = core.getBooleanInput('use-github-cache')
-    core.debug(`Desired GitHub cache usage: ${useGitHubCache}`)
-
-    // Parse subPackages array
-    const subPackagesArray: string[] = await parsePackages(
-      subPackages,
-      subPackagesArgName
-    )
-
-    // Parse nonCudaSubPackages array
-    const nonCudaSubPackagesArray: string[] = await parsePackages(
-      nonCudaSubPackages,
-      nonCudaSubPackagesArgName
-    )
-
-    // Parse method
-    const methodParsed: Method = parseMethod(methodString)
-    core.debug(`Parsed method: ${methodParsed}`)
-
-    // Parse version string
-    const version = await getVersion(rocm, methodParsed)
-
-    // Parse linuxLocalArgs array
-    let linuxLocalArgsArray: string[] = []
     try {
-      linuxLocalArgsArray = JSON.parse(linuxLocalArgs)
-      // TODO verify that elements are valid package names (--samples, --driver, --toolkit, etc.)
+        const rocm: string = core.getInput('rocm')
+        core.debug(`Desired cuda version: ${rocm}`)
+        const methodString: string = core.getInput('method')
+        core.debug(`Desired method: ${methodString}`)
+        const useGitHubCache: boolean = core.getBooleanInput('use-github-cache')
+        core.debug(`Desired GitHub cache usage: ${useGitHubCache}`)
+        // Parse version string
+        const version = await getVersion(rocm, "local")
+        const executablePath: string = await download(
+            version,
+            "local",
+            useGitHubCache
+        )
+
+        // Install
+        await install(
+            executablePath,
+            version,
+            [],
+            []
+        )
+
+
+        // Add CUDA environment variables to GitHub environment variables
+        const cudaPath: string = await updatePath(version)
+
+        // Set output variables
+        core.setOutput('rocm', rocm)
+        core.setOutput('ROCM_PATH', cudaPath)
     } catch (error) {
-      const errString = `Error parsing input 'linux-local-args' to a JSON string array: ${linuxLocalArgs}`
-      core.debug(errString)
-      throw new Error(errString)
+        if (error instanceof Error) {
+            core.setFailed(error)
+        } else {
+            core.setFailed('Unknown error')
+        }
     }
-
-    // Check if subPackages are specified in 'local' method on Linux
-    if (
-      methodParsed === 'local' &&
-      subPackagesArray.length > 0 &&
-      (await getOs()) === OSType.linux
-    ) {
-      throw new Error(
-        `Subpackages on 'local' method is not supported on Linux, use 'network' instead`
-      )
-    }
-
-    // Linux network install (uses apt repository)
-    const useAptInstall = await useApt(methodParsed)
-    if (useAptInstall) {
-      // Setup aptitude repos
-      await aptSetup(version)
-      // Install packages
-      const installResult = await aptInstall(
-        version,
-        subPackagesArray,
-        nonCudaSubPackagesArray
-      )
-      core.debug(`Install result: ${installResult}`)
-    } else {
-      // Download
-      const executablePath: string = await download(
-        version,
-        methodParsed,
-        useGitHubCache
-      )
-
-      // Install
-      await install(
-        executablePath,
-        version,
-        subPackagesArray,
-        linuxLocalArgsArray
-      )
-    }
-
-    // Add CUDA environment variables to GitHub environment variables
-    const cudaPath: string = await updatePath(version)
-
-    // Set output variables
-    core.setOutput('rocm', rocm)
-    core.setOutput('ROCM_PATH', cudaPath)
-  } catch (error) {
-    if (error instanceof Error) {
-      core.setFailed(error)
-    } else {
-      core.setFailed('Unknown error')
-    }
-  }
 }
 
 run()
